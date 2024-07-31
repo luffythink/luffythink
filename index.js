da8dc604e6d85f48a5b71397f46ccabc496d8ef6
Oscar@JING <40992395+luffythink@users.noreply.github.com>
2024-07-31 18:59:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function generateProgressBar() {
 }
 
 
-const README = `\
-
+const README.md = `\
+----------------------------
 ### 22
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ⏰ Updated on ${new Date().toUTCString()}
-
+-------------------------------
 \
 `
 
