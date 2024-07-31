50e41c937ce1c35f14bbd48b68db4d9428cf1daf
Oscar@JING <40992395+luffythink@users.noreply.github.com>
2024-07-31 19:49:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### 🚴 在迈向未来赛博新世界的路上⏳ ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+### 🚴 在迈向未来赛博新世界的路上
+### ⏳ ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 <img align="right" src="https://github-readme-stats.vercel.app/api?username=luffythink&show_icons=true&icon_color=CE1D2D&text_color=718096&bg_color=ffffff&hide_title=true"/>
 
