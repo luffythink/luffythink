7747b3e1500531451de60bb39bf1cd2f04f9a03a
Oscar@JING <40992395+luffythink@users.noreply.github.com>
2024-09-20 18:17:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const readme = `\
 
 <img align="right" src="https://github-readme-stats.vercel.app/api?username=luffythink&show_icons=true&icon_color=CE1D2D&text_color=718096&bg_color=ffffff&hide_title=true"/>
 
-- ♻️: A eco-lifelong learner. 
+- ♻️: A eco-lifelong learner & crypto explorer.
 - 😄: xipochen@Oscar
 - 🗣️: I’m currently learning Python, Blockchain...
 - ⚓: Freelancing & Cycling lover
